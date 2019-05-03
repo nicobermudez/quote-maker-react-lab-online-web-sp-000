63e661d17167073e3644305a9aad145741942b96
Nicolas Bermudez <bermnicolas@gmail.com>
2019-05-03 16:30:34 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -13,14 +13,20 @@ export default quotesReducer(state = [], action) => {
     case 'UPVOTE_QUOTE':
       index = state.findIndex(quote => quoteId === action.quoteId);
       quote = state[index];
-      return [...state.slice(0, index), Object.assign({}, quote, {votes: quote.votes += 1}),
+      return [
+      ...state.slice(0, index), Object.assign({}, quote, {votes: quote.votes += 1}),
       ...state.slice(index + 1)
-    ];
+      ];
 
     case 'DOWNVOTE_QUOTE':
       index = state.findIndex(quote => quoteId === action.quoteId);
       quote = state[index];
-      
+      if(quote.votes > 0) {
+        return [
+        ...state.slice(0, index), Object.assign({}, quote, {votes: quote.votes -= 1}),
+        ...state.slice(index + 1)
+        ];
+      }
 
     default:
       return state;
