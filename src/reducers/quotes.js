94ecae86cea736f96da49118c6c78d6021dba147
Nicolas Bermudez <bermnicolas@gmail.com>
2019-05-03 16:21:41 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,8 +1,11 @@
 export default quotesReducer(state = [], action) => {
   switch(action.type) {
     case 'ADD_QUOTE':
+
     case 'REMOVE_QUOTE':
     case 'UPVOTE_QUOTE':
     case 'DOWNVOTE_QUOTE':
+    default:
+      return state;
   }
 }
