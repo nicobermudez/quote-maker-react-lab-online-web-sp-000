465aab3c25e289577868d5649e5cf83fda6b9525
Nicolas Bermudez <bermnicolas@gmail.com>
2019-05-03 16:57:03 +0000

    Automatically backed up by Learn

--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -3,18 +3,19 @@ import uuid from 'uuid';
 import { connect } from 'react-redux';
 import { addQuote } from '../actions/quotes';
 
+
 class QuoteForm extends Component {
 
   state = {
-    content: "",
-    author: ""
+    content: '',
+    author: ''
   }
 
   handleOnChange = event => {
     const { value, name } = event.target;
     this.setState({
       [name]: value
-    })
+    });
   }
 
   handleOnSubmit = event => {
@@ -24,7 +25,7 @@ class QuoteForm extends Component {
     this.setState({
       content: '',
       author: ''
-    })
+    });
   }
 
   render() {
@@ -34,12 +35,13 @@ class QuoteForm extends Component {
           <div className="col-md-8 col-md-offset-2">
             <div className="panel panel-default">
               <div className="panel-body">
-                <form className="form-horizontal">
+                <form className="form-horizontal" onSubmit={this.handleOnSubmit}>
                   <div className="form-group">
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
                       <textarea
                         className="form-control"
+                        name="content"
                         value={this.state.content}
                         onChange={this.handleOnChange}
                       />
@@ -51,6 +53,7 @@ class QuoteForm extends Component {
                       <input
                         className="form-control"
                         type="text"
+                        name="author"
                         value={this.state.author}
                         onChange={this.handleOnChange}
                       />
@@ -70,6 +73,5 @@ class QuoteForm extends Component {
     );
   }
 }
-
 
 export default connect(null, { addQuote })(QuoteForm);
