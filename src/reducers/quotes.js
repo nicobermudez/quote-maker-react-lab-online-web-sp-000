216c4329e872796678d5bc855e72330ec3540bb4
Nicolas Bermudez <bermnicolas@gmail.com>
2019-05-03 16:53:05 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -11,7 +11,7 @@ export default (state = [], action) => {
       return state.filter;(quote => quote.id !== action.quoteId)
 
     case 'UPVOTE_QUOTE':
-      index = state.findIndex(quote => quoteId === action.quoteId);
+      index = state.findIndex(quote => quote.id === action.quoteId);
       quote = state[index];
       return [
       ...state.slice(0, index), Object.assign({}, quote, {votes: quote.votes += 1}),
