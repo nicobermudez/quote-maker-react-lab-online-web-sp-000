531edc815113028f153d50f021552dd27c580b48
Nicolas Bermudez <bermnicolas@gmail.com>
2019-05-03 16:43:18 +0000

    Automatically backed up by Learn

--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -25,7 +25,6 @@ class QuoteForm extends Component {
       content: '',
       author: ''
     })
-
   }
 
   render() {
@@ -72,5 +71,5 @@ class QuoteForm extends Component {
   }
 }
 
-//add arguments to connect as needed
-export default connect()(QuoteForm);
+
+export default connect(null, { addQuote })(QuoteForm);
